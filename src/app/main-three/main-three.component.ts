796c806040d53abc7fba916c5bad32ef54d5fa4b
jordi <jicasares94@gmail.com>
2020-04-26 21:31:03 +0200

    Editor Mode

--- src/app/main-three/main-three.component.ts
+++ src/app/main-three/main-three.component.ts
@@ -6,11 +6,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { GUI } from 'three/examples/jsm/libs/dat.gui.module';
 
-// Interfaces
-import { InstanceObject } from './objectInstance';
-
 @Component({
-  selector: 'main-three',
+  selector: 'app-main-three',
   templateUrl: './main-three.component.html',
   styleUrls: ['./main-three.component.scss']
 })
@@ -20,78 +17,95 @@ export class MainThreeComponent implements OnInit, AfterViewInit {
   canvas: HTMLCanvasElement;
 
   options = {
-    'Wireframe': true,
-    'Autorotation': true,
-  }
+    Wireframe: true,
+  };
 
   scene: THREE.Scene = new THREE.Scene();
-  camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+  camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 100);
   renderer: THREE.WebGLRenderer = null;
+
   mouse: THREE.Vector2 = new THREE.Vector2(1, 1);
   raycaster: THREE.Raycaster = new THREE.Raycaster();
   controls: OrbitControls = null;
   stats: Stats = Stats();
-  gui: GUI = new GUI();
+  // gui: GUI = new GUI();
 
-  objects: Array<THREE.Group> = [];
+  cube: THREE.Mesh;
+  line: THREE.Line;
 
   constructor() {
   }
 
   ngOnInit(): void {
-  }
+    document.body.appendChild(this.stats.dom);
 
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    this.camera.aspect = width / height;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(width, height);
+    // TODO: Wireframe Option
+    // this.gui.add(this.options, 'Wireframe').onChange(() => {
+    //   this.objects.forEach(element => element.children[0].visible = this.options.Wireframe);
+    // });
   }
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(event) {
     event.preventDefault();
 
-    this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    this.mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+    this.mouse.x = (event.clientX / this.canvas.clientWidth) * 2 - 1;
+    this.mouse.y = - (event.clientY / this.canvas.clientHeight) * 2 + 1;
   }
 
-  private createInstance(input: InstanceObject): THREE.Group {
-    const group = new THREE.Group();
+  @HostListener('document:mousedown', [])
+  onMouseDown() {
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    const intersection = this.raycaster.intersectObject(this.cube);
 
+    if (intersection.length > 0) {
+      const face = intersection[ 0 ].face;
+      // @ts-ignore
+      const linePosition = this.line.geometry.attributes.position;
+      // @ts-ignore
+      const cubePosition = this.cube.geometry.attributes.position;
+
+      /**
+       * https://threejs.org/docs/#api/en/core/BufferAttribute
+       * 'copyAt' copy a vector from a bufferAttribute[index2] to a Array[index1].
+       * 'applyMatrix4' applies matrix to every Vector3 element.
+       */
+      linePosition.copyAt( 0, cubePosition, face.a );
+      linePosition.copyAt( 1, cubePosition, face.b );
+      linePosition.copyAt( 2, cubePosition, face.c );
+      linePosition.copyAt( 3, cubePosition, face.a );
+      this.line.geometry.applyMatrix4( this.cube.matrix );
+
+      // https://threejsfundamentals.org/threejs/lessons/threejs-optimize-lots-of-objects.html
+      // this.cube.geometry.applyMatrix4(new THREE.Matrix4().makeTranslation(0, 0, 0.5));
+
+      this.line.visible = true;
+    }
+  }
+
+  private createInstance(geometry): void {
     // Color Material Mesh
     const material = new THREE.MeshPhongMaterial({
-      color: input.color,
+      wireframe: false,
+      color: 0x555555,
+      specular: 0xffffff,
+      shininess: 50,
       polygonOffset: true,
       polygonOffsetFactor: 1,
       polygonOffsetUnits: 1
     });
-    const colorMesh = new THREE.Mesh(input.geometry, material);
-    colorMesh.name = 'Color';
-    group.add(colorMesh);
 
-    // Wireframe Mesh
-    const wireframeMat = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-    const wireframeMesh = new THREE.Mesh(input.geometry, wireframeMat);
-    wireframeMesh.name = 'Wireframe';
-    wireframeMesh.visible = this.options.Wireframe;
-    group.add(wireframeMesh);
-
-    // Set position & Add into Scene
-    group.position.x = input.x_position;
-    this.scene.add(group);
-
-    return group
+    this.cube = new THREE.Mesh(geometry, material);
+    this.scene.add(this.cube);
   }
 
   private render(): void {
-    this.raycaster.setFromCamera(this.mouse, this.camera);
-    const intersection = this.raycaster.intersectObject(this.objects[0].children[0]);
-
-    if (intersection.length > 0) {
-      console.debug('Intersection!', intersection);
+    const width = this.canvas.clientWidth;
+    const height = this.canvas.clientHeight;
+    if ( this.canvas.width !== width || this.canvas.height !== height ) {
+      this.camera.aspect = width / height;
+      this.camera.updateProjectionMatrix();
+      this.renderer.setSize(width, height, false);
     }
 
     this.controls.update();
@@ -104,38 +118,33 @@ export class MainThreeComponent implements OnInit, AfterViewInit {
     /**
      * Setup Canvas + Controls + Camera
      */
-    this.canvas = <HTMLCanvasElement>this.canvasElement.nativeElement;
-
-    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas, antialias: true, alpha: false });
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: (this.canvasElement.nativeElement as HTMLCanvasElement),
+      antialias: true,
+      alpha: true
+    });
+    this.canvas = this.renderer.domElement;
 
     this.controls = new OrbitControls(this.camera, this.canvas);
     this.camera.position.set(2, 2, 3);
     this.camera.lookAt(0, 0, 0);
 
-    // Stats + GUI
-    document.body.appendChild(this.stats.dom);
-
-    this.gui.add(this.options, 'Wireframe').onChange(() => {
-      this.objects.forEach(element => {
-        const found = element.children.find(child => child.name === "Wireframe");
-        found.visible = this.options.Wireframe
-      });
-    });
-
     /**
      * Setup Geometry and Lighting
      */
-    const newObj: InstanceObject = {
-      geometry: new THREE.BoxGeometry,
-      color: 0x44aa88,
-      x_position: 0
-    }
-    this.objects.push(this.createInstance(newObj));
 
-    const light: THREE.DirectionalLight = new THREE.DirectionalLight(0xFFFFFF, 1);
-    light.position.set(0, 2, 4);
-    this.scene.add(light);
+    // Selection Triangle!!!
+    const triangleGeo = new THREE.BufferGeometry();
+    triangleGeo.setAttribute('position', new THREE.BufferAttribute( new Float32Array( 4 * 3 ), 3 ));
+    const triangleMat = new THREE.LineBasicMaterial({ color: 0x00FF00, transparent: true });
+    this.line = new THREE.Line( triangleGeo, triangleMat );
+    this.scene.add(this.line);
+
+    // Grid Helper
+    const gridHelper = new THREE.GridHelper( 10, 20 );
+    this.scene.add( gridHelper );
+
+    this.createInstance(new THREE.BoxBufferGeometry());
 
     requestAnimationFrame(this.render.bind(this));
   }
